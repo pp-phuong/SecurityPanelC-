f9f320e57c220a35311547ac81c571ec3f311080
Trần Thị Phượng <tranphiphuong2763@gmail.com>
2021-03-29 09:28:31 +0700

    security panel c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace SecurityPanel
+{
+    partial class SecurityPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.text_pass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "Button_Click";
+            this.button1.BackColor = System.Drawing.Color.BurlyWood;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(76, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "Button_Click";
+            this.button2.BackColor = System.Drawing.Color.BurlyWood;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(144, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 46);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            this.button2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button3
+            // 
+            this.button3.AccessibleName = "Button_Click";
+            this.button3.BackColor = System.Drawing.Color.BurlyWood;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(214, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 46);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            this.button3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button4
+            // 
+            this.button4.AccessibleName = "Button_Click";
+            this.button4.BackColor = System.Drawing.Color.BurlyWood;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(76, 152);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 46);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            this.button4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button5
+            // 
+            this.button5.AccessibleName = "Button_Click";
+            this.button5.BackColor = System.Drawing.Color.BurlyWood;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(144, 152);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 46);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            this.button5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button6
+            // 
+            this.button6.AccessibleName = "Button_Click";
+            this.button6.BackColor = System.Drawing.Color.BurlyWood;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(214, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 46);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            this.button6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button7
+            // 
+            this.button7.AccessibleName = "Button_Click";
+            this.button7.BackColor = System.Drawing.Color.BurlyWood;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(76, 204);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(49, 46);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            this.button7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button8
+            // 
+            this.button8.AccessibleName = "Button_Click";
+            this.button8.BackColor = System.Drawing.Color.BurlyWood;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(144, 204);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(49, 46);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            this.button8.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button9
+            // 
+            this.button9.AccessibleName = "Button_Click";
+            this.button9.BackColor = System.Drawing.Color.BurlyWood;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(214, 204);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(49, 46);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            this.button9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // button11
+            // 
+            this.button11.AccessibleName = "Button_Click";
+            this.button11.BackColor = System.Drawing.Color.BurlyWood;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(144, 256);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(49, 46);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "0";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button1_Click);
+            this.button11.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "login log"});
+            this.listBox1.Location = new System.Drawing.Point(76, 343);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(187, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Security Panel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "login log";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Gold;
+            this.btnClear.Location = new System.Drawing.Point(76, 256);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(49, 46);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "AC";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnConfirm.Location = new System.Drawing.Point(214, 256);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(49, 46);
+            this.btnConfirm.TabIndex = 6;
+            this.btnConfirm.Text = "#";
+            this.btnConfirm.UseVisualStyleBackColor = false;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            this.btnConfirm.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pressNumber);
+            // 
+            // text_pass
+            // 
+            this.text_pass.Enabled = false;
+            this.text_pass.Location = new System.Drawing.Point(77, 61);
+            this.text_pass.Name = "text_pass";
+            this.text_pass.PasswordChar = '*';
+            this.text_pass.Size = new System.Drawing.Size(186, 20);
+            this.text_pass.TabIndex = 7;
+            // 
+            // SecurityPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(348, 450);
+            this.Controls.Add(this.text_pass);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "SecurityPanel";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.TextBox text_pass;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SecurityPanel
+{
+    public partial class SecurityPanel : Form
+    {
+        string path = @"C:\Users\Em\source\repos\Baitap\log.txt";
+        string pass = "1234";
+        public SecurityPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (text_pass.Text.Length < 4)
+            {
+                text_pass.Text += ((Button)sender).Text;
+            }
+            btnConfirm.Focus();
+
+        }
+        private void pressNumber(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) btnClear_Click(sender, e);
+            if (text_pass.Text.Length < 4 && e.KeyChar <= '9' && e.KeyChar >= '0')
+            {
+                text_pass.Text += e.KeyChar.ToString();
+            }
+            btnConfirm.Focus();
+        }
+        public void pressEnter()
+        {
+            DateTime now = DateTime.Now;
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                if (text_pass.Text == pass)
+                {
+
+                    sw.WriteLine(now + "    Login success");
+                    MessageBox.Show("Đăng nhập thành công", "Login Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    text_pass.Text = "";
+                }
+                else
+                {
+                    sw.WriteLine(now + "    Login fail");
+                    MessageBox.Show("Đăng nhập không thành công", "Login Fail", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    text_pass.Text = "";
+                }
+            }
+            Log();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Log();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            text_pass.Text = "";
+            btnConfirm.Focus();
+
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                if (text_pass.Text == pass)
+                {
+
+                    sw.WriteLine(now + "    Login success");
+                    MessageBox.Show("Đăng nhập thành công", "Login Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    text_pass.Text = "";
+                }
+                else
+                {
+                    sw.WriteLine(now + "    Login fail");
+                    MessageBox.Show("Đăng nhập không thành công", "Login Fail", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    text_pass.Text = "";
+                }
+            }
+            Log();
+        }
+
+        public void Log()
+        {
+            listBox1.Items.Clear();
+            string[] log = File.ReadAllLines(path);
+            foreach (string line in log)
+            {
+                listBox1.Items.Add(line);
+            }
+        }
+    }
+}
